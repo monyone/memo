2272afc0e87ba785771da03e007690bcaaad78d4
monyone <monyone.teihen@gmail.com>
2024-04-24 10:38:00 +0900

    Feat: Add Creation/Modification Data

--- src/app/(components)/(organization)/card.tsx
+++ src/app/(components)/(organization)/card.tsx
@@ -5,21 +5,35 @@ import React from 'react';
 type Post = {
   href: string,
   title: string,
-  description: string
+  description: string,
+  created: string,
+  modified: string,
 }
 
-export default function Card({ href, title, description }: Post) {
+export default function Card({ href, title, description, created, modified }: Post) {
   return (
     <div className='flex flex-col rounded-lg bg-slate-100' role='presentation none'>
       <Link className="block h-full" href={href}>
-        <div className='flex flex-col p-4' role='presentation none'>
-          <div className='flex font-semibold' role='presentation none'>
-            {title}
+        <div className='flex flex-col p-4 h-full' role='presentation none'>
+          <div className='flex flex-col grow' role='presentation none'>
+            <div className='flex font-semibold mb-2' role='presentation none'>
+              {title}
+            </div>
+            <div className='flex grow my-2' role='presentation none'>
+              <p className='text-sm'>
+                { description }
+              </p>
+            </div>
           </div>
-          <div className='flex' role='presentation none'>
-            <p className='text-sm'>
-              { description }
-            </p>
+          <div className='flex flex-row-reverse mt-4'>
+            <div className='flex flex-col text-xs ml-full'>
+              <div className='inline-block'>
+                { created } 作成
+              </div>
+              <div className='inline-block'>
+                { modified } 編集
+              </div>
+            </div>
           </div>
         </div>
       </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import fs from 'fs';
 import { glob } from 'glob';
 import path from 'path';
 import matter from 'gray-matter';
+import { execSync } from 'child_process';
+import dayjs from 'dayjs';
 
 import Card from '@/app/(components)/(organization)/card';
 
@@ -10,17 +12,26 @@ export default async function Home() {
   const pages = (await glob(path.join(... BLOG_DIR, `*`, `index.md`))).map((p) => {
       const { data } = matter(fs.readFileSync(p));
       const relative = path.relative(path.join(... BLOG_DIR), p);
+      const created = dayjs(execSync(`git log --follow --format=%cI --reverse --date default ${p}`).toString('utf-8').trim().split('\n')[0]);
+      const modified = dayjs(execSync(`git log --follow --format=%cI --date default ${p}`).toString('utf-8').trim().split('\n')[0]);
+
       return {
+        path: p,
         href: path.join(path.dirname(relative), 'index.html'),
         title: data.title as string,
-        description: data.description as string
+        description: data.description as string,
+        created: created.format('YYYY/MM/DD'),
+        modified: modified.format('YYYY/MM/DD'),
+        hidden: data.hidden!!,
+        order: created.unix()
       }
-    });
+    }).filter(({ hidden }) => !hidden);
+  pages.sort((a, b) => a.order - b.order);
 
   return (
     <div className='px-4'>
       <div className='grid grid-cols-3 gap-4'>
-        { pages.map((page) => <Card {... page } />) }
+        { pages.map((page) => <Card {... page } key={page.path} />) }
       </div>
     </div>
   );
