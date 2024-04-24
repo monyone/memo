5a051f8c3f5eed12c1f762e297cc557245653cd0
monyone <monyone.teihen@gmail.com>
2024-04-25 06:43:50 +0900

    Fix: Fallback to not found image src

--- src/app/(components)/(markdoc)/image.tsx
+++ src/app/(components)/(markdoc)/image.tsx
@@ -8,19 +8,23 @@ export default async function Image({ src, alt, title }: { src: string, alt?: st
     );
   }
 
-  const data = await readFile(src);
-  const ext = path.extname(src).slice(1);
-  if (ext === 'svg') {
-    const datauri = `data:image/${ext}+xml;utf-8,${encodeURIComponent(data.toString('utf-8'))}`;
+  try {
+    const data = await readFile(src);
+    const ext = path.extname(src).slice(1);
+    if (ext === 'svg') {
+      const datauri = `data:image/${ext}+xml;utf-8,${encodeURIComponent(data.toString('utf-8'))}`;
+      return (
+        <img src={datauri} alt={alt} title={title} className='my-3 mx-auto' />
+      );
+    }
+
+    const datauri = `data:image/${ext};base64,${data.toString('base64')}`;
     return (
       <img src={datauri} alt={alt} title={title} className='my-3 mx-auto' />
     );
+  } catch(e: unknown) {
+    return <img alt={alt} title={title} className='my-3 mx-auto' />
   }
-
-  const datauri = `data:image/${ext};base64,${data.toString('base64')}`;
-  return (
-    <img src={datauri} alt={alt} title={title} className='my-3 mx-auto' />
-  );
 };
 
 export function ImageWithSlug(slug: string) {
